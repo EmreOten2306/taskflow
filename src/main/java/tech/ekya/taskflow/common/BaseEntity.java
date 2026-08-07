dd043f859e041f1fd3dffde7c39e23a350e98e6d
Emre  Öten <emreoten@bitaksis-MacBook-Pro.local>
2026-08-07 16:53:17 -0700

    Create BaseEntity with audit fields

---
+++ src/main/java/tech/ekya/taskflow/common/BaseEntity.java
@@ -0,0 +1,34 @@
+package tech.ekya.taskflow.common;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import java.time.LocalDateTime;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+
+@MappedSuperclass
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+
+    @PrePersist
+    private void prePersist() {
+
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+        @PreUpdate
+        public void preUpdate() {
+            updatedAt = LocalDateTime.now();
+
+    }
+}
+
