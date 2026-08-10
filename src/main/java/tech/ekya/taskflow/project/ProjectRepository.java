e76c21a7fa0120eb34c396696883c4695d565b66
Emre  Öten <emreoten@bitaksis-MacBook-Pro.local>
2026-08-10 12:36:44 -0700

    add user and project repositories

---
+++ src/main/java/tech/ekya/taskflow/project/ProjectRepository.java
@@ -0,0 +1,6 @@
+package tech.ekya.taskflow.project;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjectRepository extends JpaRepository<Project, Long> {
+}
---
+++ src/main/java/tech/ekya/taskflow/user/AppUserRepository.java
@@ -0,0 +1,7 @@
+package tech.ekya.taskflow.user;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+public interface AppUserRepository extends JpaRepository<AppUser ,Long> {
+}
