b5217cf0b7deee7b7fc5b81c358cf6efcac5d13f
Emre Öten <Eoten@freshdirect.com>
2026-08-25 10:44:50 -0700

    feat(user): add AppUser CRUD operations

--- src/main/java/tech/ekya/taskflow/user/AppUserController.java
+++ src/main/java/tech/ekya/taskflow/user/AppUserController.java
@@ -1,19 +1,35 @@
 package tech.ekya.taskflow.user;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/users")
 public class AppUserController {
 
     private final AppUserService appUserService;
     public AppUserController(AppUserService appUserService){
         this.appUserService = appUserService;
     }
-    @GetMapping("/api/users")
+
+    @GetMapping
     public List<AppUser> getAllUsers() {
         return appUserService.getAllUsers();
+    }
+
+    @GetMapping ("/{id}")
+    public AppUser getUserById(@PathVariable Long id){
+        return appUserService.getUserById(id);
+    }
+    @PutMapping ("/{id}")
+    public AppUser updateUser(@PathVariable Long id,
+                              @RequestBody AppUser appUser){
+        return appUserService.updateUser(id, appUser);
+    }
+
+    @DeleteMapping ("/{id}")
+    public void deleteUserById(@PathVariable Long id){
+        appUserService.deleteUserById(id);
     }
 }
--- src/main/java/tech/ekya/taskflow/user/AppUserService.java
+++ src/main/java/tech/ekya/taskflow/user/AppUserService.java
@@ -1,6 +1,7 @@
 package tech.ekya.taskflow.user;
 
 import org.springframework.stereotype.Service;
+import tech.ekya.taskflow.exception.ResourceNotFoundException;
 
 import java.util.List;
 
@@ -10,7 +11,43 @@ public class AppUserService {
     public AppUserService(AppUserRepository appUserRepository){
         this.appUserRepository = appUserRepository;
     }
+
+    ///GET ALL
     public List<AppUser> getAllUsers(){
         return appUserRepository.findAll();
+    }
+
+    ///GET BY ID
+    public AppUser getUserById(Long id){
+        return appUserRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException(
+                        "User not found with id: " + id
+                ));
+    }
+
+
+    
+
+    ///UPDATE BY ID
+    public AppUser updateUser(Long id ,AppUser appUser){
+        AppUser existingUser = appUserRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException(
+                        "User not found with id: " + id
+                ));
+        existingUser.setFullName(appUser.getFullName());
+        existingUser.setEmail(appUser.getEmail());
+        existingUser.setRole(appUser.getRole());
+        existingUser.setPasswordHash(appUser.getPasswordHash());
+
+        return appUserRepository.save(existingUser);
+    }
+
+    /// DELETE
+    public void deleteUserById(Long id){
+        AppUser appUser = appUserRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException(
+                        "User not found with id: " + id
+                ));
+        appUserRepository.deleteById(id);
     }
 }
