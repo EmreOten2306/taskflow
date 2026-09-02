a80f8030b75e81bc5d92196cd86fb250a07cba23
Emre Öten <Eoten@freshdirect.com>
2026-09-02 14:21:50 -0700

    chore: update SQL seed and run-all scripts

--- sql/03_seed_performance_data.sql
+++ sql/03_seed_performance_data.sql
@@ -1,32 +1,37 @@
-INSERT INTO task (
-                  created_at,
-                  updated_at,
-                  description,
-                  due_date,
-                  priority,
-                  status,
-                  title,
-                  assignee_id,
-                  project_id,
-                  completed_at
+WITH inserted_tasks AS (
+    INSERT INTO task (
+                      created_at,
+                      updated_at,
+                      description,
+                      due_date,
+                      priority,
+                      status,
+                      title,
+                      assignee_id,
+                      project_id,
+                      completed_at
+        )
+        SELECT
+            now() - (i || ' minutes')::interval,
+            now(),
+            'Performance test task',
+            CURRENT_DATE + ((i % 120) - 60),
+            (ARRAY['LOW','MEDIUM','HIGH','CRITICAL'])[1 + (i % 4)],
+            (ARRAY['TODO','IN_PROGRESS','IN_REVIEW','DONE'])[1 + (i % 4)],
+            'Task #' || i,
+            CASE
+                WHEN i % 7 = 0 THEN NULL
+                ELSE (ARRAY[1,2,4])[1 + (i % 3)]
+                END,
+            (ARRAY[29,30,31,33,34,35,36,37,38])[1 + (i % 9)],
+            CASE
+                WHEN i % 4 = 1
+                    THEN now() - ((i % 56) || ' days')::interval
+                ELSE NULL
+                END
+        FROM generate_series(1, 50000) AS s(i)
+        RETURNING id
 )
-SELECT
-    now() - (i || ' minutes')::interval,
-       now(),
-       'Performance test task',
-       CURRENT_DATE + ((i % 120) - 60),
-       (ARRAY['LOW','MEDIUM','HIGH','CRITICAL'])[1 + (i % 4)],
-       (ARRAY['TODO','IN_PROGRESS','IN_REVIEW','DONE'])[1 + (i % 4)],
-       'Task #' || i,
-    CASE
-        WHEN i % 7 = 0 THEN NULL
-        ELSE (ARRAY[1,2,4])[1 + (i % 3)]
-        END,
-    (ARRAY[29,30,31,33,34,35,36,37,38])[1 + (i % 9)],
-       CASE WHEN i % 4 = 1
-         THEN now() - ((i % 56) || ' days')::interval
-            ELSE NULL END
-FROM generate_series(1, 50000) AS s(i);
 
 INSERT INTO comment (
     created_at,
@@ -40,8 +45,9 @@ SELECT
     now(),
     'Performance test comment',
     2,
-    100013 + ((i - 1) % 50000)
-FROM generate_series(1, 100000) AS s(i);
+    inserted_tasks.id
+FROM inserted_tasks
+         CROSS JOIN generate_series(1, 2);
 
 EXPLAIN (ANALYZE, BUFFERS)
 SELECT
